13fc0e3b70ce37b35d5adc76a9a0b52f75e15960
burhan-numar <burhan.numar1-tech.com>
2025-08-05 15:25:53 +0300

    Dosyalar temizlendi

--- src/test/java/AllPageTest.java
+++ src/test/java/AllPageTest.java
@@ -1,2 +1,2 @@
-package PACKAGE_NAME;public class AllPageTest {
+public class AllPageTest {
 }
--- src/test/java/TimeOut.java
+++ src/test/java/TimeOut.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public enum TimeOut {
+/**
+ * Test timeout değerleri için enum
+ */
+public enum TimeOut {
+    SHORT(5),
+    MEDIUM(10),
+    LONG(15),
+    EXTRA_LONG(30),
+    PAGE_LOAD(60);
+
+    public final int value;
+
+    TimeOut(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
 }
